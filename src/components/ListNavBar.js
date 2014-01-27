da5983d102828d641c14072adac56ff4b594128d
Jamie <jamie@tidepool.org>
2014-01-27 21:58:04 +1300

    Adding license

--- src/components/ListNavBar.js
+++ src/components/ListNavBar.js
@@ -1,4 +1,23 @@
-/** @jsx React.DOM */
+/**
+ * @jsx React.DOM
+ */
+
+/*
+== BSD2 LICENSE ==
+Copyright (c) 2014, Tidepool Project
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the associated License, which is identical to the BSD 2-Clause
+License as published by the Open Source Initiative at opensource.org.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the License for more details.
+
+You should have received a copy of the License along with this program; if
+not, you can obtain one from Tidepool Project at tidepool.org.
+== BSD2 LICENSE ==
+*/
 
 var React = require('react');
 
@@ -13,7 +32,7 @@ var ListNavBar = React.createClass({
             /* jshint ignore:start */
             <nav className='navbar navbar-default navbar-fixed-top' role='navigation'>
                 <button type='button' ref='actionBtn' className='btn btn-default navbar-btn col-xs-1' onClick={this.handleAction}><span className={this.props.actionIcon}></span></button>
-                <p className='navbar-text text-center col-xs-8'>{this.props.title}</p>
+                <h4 className='navbar-text text-center col-xs-8'>{this.props.title}</h4>
                 <div className='col-xs-2 pull-right'>
                     {this.props.children}
                 </div>
