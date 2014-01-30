d60c8ae5516a8ddab8be35d864603bd9d854636c
Jamie <jamie@tidepool.org>
2014-01-30 15:32:12 +1300

    go to new page when adding a new message

--- src/components/MessageFooter.js
+++ src/components/MessageFooter.js
@@ -19,34 +19,36 @@ not, you can obtain one from Tidepool Project at tidepool.org.
 == BSD2 LICENSE ==
 */
 
+'use strict';
+
 var React = require('react');
 
 //Form for adding Comments
 var MessageFooter = React.createClass({
 
-    handleSubmit: function() {
-        var messageText = this.refs.messageText.getDOMNode().value.trim();
-        this.props.onFooterAction({text: messageText});
-        this.refs.messageText.getDOMNode().value = '';
-        return false;
-    },
+  handleSubmit: function() {
+    var messageText = this.refs.messageText.getDOMNode().value.trim();
+    this.props.onFooterAction({text: messageText});
+    this.refs.messageText.getDOMNode().value = '';
+    return false;
+  },
 
-    render: function() {
+  render: function() {
 
-        return this.transferPropsTo(
-            /* jshint ignore:start */
-            <nav className="navbar navbar-default navbar-fixed-bottom">
-                <form className="navbar-form">
-                    <div className="form-group col-xs-10">
-                        <input type="text" className="form-control " ref='messageText' placeholder={this.props.messagePrompt} />
-                    </div>
-            
-                    <button type="submit" ref='sendBtn' className="btn btn-default col-xs-2" onClick={this.handleSubmit}>{this.props.btnMessage}</button>
-                </form>
-            </nav>
-            /* jshint ignore:end */ 
-        );
-    }
+    return this.transferPropsTo(
+      /* jshint ignore:start */
+      <nav className='navbar navbar-default navbar-fixed-bottom'>
+      <form className='navbar-form'>
+        <div className='form-group col-xs-10'>
+          <input type='text' className='form-control' ref='messageText' onFocus={this.handleSubmit} placeholder={this.props.messagePrompt} />
+        </div>
+
+        <button type='submit' ref='sendBtn' className='btn btn-default col-xs-2' onClick={this.handleSubmit}>{this.props.btnMessage}</button>
+      </form>
+      </nav>
+      /* jshint ignore:end */
+      );
+  }
 });
 
 module.exports = MessageFooter;
