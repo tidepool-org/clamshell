eea101a688fecd7dceb0647a04370880adc78f58
Jamie <jamie@tidepool.org>
2014-02-16 15:30:43 +1300

    jshint for user

--- src/core/user.js
+++ src/core/user.js
@@ -14,47 +14,48 @@ You should have received a copy of the License along with this program; if
 not, you can obtain one from Tidepool Project at tidepool.org.
 == BSD2 LICENSE ==
 */
+'use strict';
 
 var _ = require('underscore');
 
 var user = {
-    validate: function(attributes, options) {
-        options = options || {};
-        var ignoreMissingAttributes = true;
-        if (options.ignoreMissingAttributes === false) {
-            ignoreMissingAttributes = false;
-        }
-        var errors = {};
-
-        var needsValidation = function(attributeName) {
-            if (ignoreMissingAttributes && !_.has(attributes, attributeName)) {
-                return false;
-            }
-            return true;
-        };
-
-        if (needsValidation('username')) {
-            errors.username = this.validateRequired(attributes.username);
-        }
-        if (needsValidation('password')) {
-            errors.password = this.validateRequired(attributes.password);
-        }
-
-        // Filter "empty" errors
-        errors = _.transform(errors, function(result, value, key) {
-            if (value) {
-                result[key] = value;
-            }
-        });
-
-        return errors;
-    },
-
-    validateRequired: function(value) {
-        if (!value) {
-            return 'This field is required.';
-        }
+  validate: function(attributes, options) {
+    options = options || {};
+    var ignoreMissingAttributes = true;
+    if (options.ignoreMissingAttributes === false) {
+      ignoreMissingAttributes = false;
     }
+    var errors = {};
+
+    var needsValidation = function(attributeName) {
+      if (ignoreMissingAttributes && !_.has(attributes, attributeName)) {
+        return false;
+      }
+      return true;
+    };
+
+    if (needsValidation('username')) {
+      errors.username = this.validateRequired(attributes.username);
+    }
+    if (needsValidation('password')) {
+      errors.password = this.validateRequired(attributes.password);
+    }
+
+    // Filter "empty" errors
+    errors = _.transform(errors, function(result, value, key) {
+      if (value) {
+        result[key] = value;
+      }
+    });
+
+    return errors;
+  },
+
+  validateRequired: function(value) {
+    if (!value) {
+      return 'This field is required.';
+    }
+  }
 };
 
 module.exports = user;
