219fcd4bbf3ffe22e46789552049310e6a0005af
Jamie <jamie@tidepool.org>
2014-02-17 11:38:51 +1300

    add user message - for displaying errors while running

---
+++ src/components/UserMessage.js
@@ -0,0 +1,42 @@
+/**
+ * @jsx React.DOM
+ */
+
+/*
+== BSD2 LICENSE ==
+Copyright (c) 2014, Tidepool Project
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the associated License, which is identical to the BSD 2-Clause
+License as published by the Open Source Initiative at opensource.org.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the License for more details.
+
+You should have received a copy of the License along with this program; if
+not, you can obtain one from Tidepool Project at tidepool.org.
+== BSD2 LICENSE ==
+*/
+
+'use strict';
+
+var React = require('react');
+
+require('./UserMessage.css');
+
+var UserMessage = React.createClass({
+
+  render: function() {
+
+    return (
+       /* jshint ignore:start */
+      <div className='login-message col-sm-offset-2 col-sm-3 js-login-message'>
+        this.props.userMessage;
+      </div>
+       /* jshint ignore:end */
+      );
+  }
+});
+
+module.exports = UserMessage;
