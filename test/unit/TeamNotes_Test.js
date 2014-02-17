1db109787681d3a8d8031dd7ba8e3e9716ca44f7
Jamie <jamie@tidepool.org>
2014-02-17 16:03:26 +1300

    remove tests that don apply, update props

--- test/unit/TeamNotes_Test.js
+++ test/unit/TeamNotes_Test.js
@@ -28,8 +28,6 @@ var handlerCalled = false;
 var propsGiven;
 
 var handleThreadSelected = function(props, key){
-  console.log('props: ',props);
-  console.log('key: ',key);
   propsGiven = props;
   handlerCalled = true;
 };
@@ -52,54 +50,34 @@ describe('TeamNotes component', function() {
     document.documentElement.removeChild(container);
   });
 
-  it('should have method to get a nice time for display from the timestamp', function() {
-    expect(component.niceTime).to.exist;
-  });
-
-  it('should have return the date to be displayed', function() {
-    var dateString = '2013-12-24T23:07:40+00:00';
-    var dateToDisplay = component.niceTime(dateString);
-    expect(dateToDisplay).to.equal(dateString);
-  });
-
   it('should call handler for group selection when a note is clicked', function() {
     //call the onClick of first groupitem that is a child of our component
-    component.refs.groupNote.props.onClick();
+    component.refs.teamNote.props.onClick();
     expect(handlerCalled).to.be.true;
   });
 
   it('should return the note when it is clicked', function() {
     //call the onClick of first groupitem that is a child of our component
-    component.refs.groupNote.props.onClick();
+    component.refs.teamNote.props.onClick();
     expect(propsGiven).to.have.id;
     expect(propsGiven).to.have.messagetext;
     expect(propsGiven).to.have.groupid;
-    expect(propsGiven).to.have.rootmessageid;
     expect(propsGiven).to.have.userid;
     expect(propsGiven).to.have.timestamp;
   });
 
   it('should return two notes', function() {
     //two notes but also replies/comments to those notes
-    var group = groups[0];
-    var converstions = component.notesForGroup(group);
-    expect(converstions.length).to.equal(2);
-  });
-
-  it('should return all messages when there are no replies', function() {
-    //added test as all messages are root 'notes' with no replies
-    var group = groups[1];
-    var converstions = component.notesForGroup(group);
-    expect(converstions.length).to.equal(3);
+    var notes = component.notesForGroup(team);
+    expect(notes.length).to.equal(2);
   });
 
   it('should have two notes where the key is the id for the root message of each thread', function() {
     //call the onClick of first groupitem that is a child of our component
-    var group = groups[0];
-    var converstions = component.notesForGroup(group);
+    var notes = component.notesForGroup(team);
 
-    expect(converstions[0].props.key).to.equal('9233c2ae-7bad-41f5-9295-e73f0437295b');
-    expect(converstions[1].props.key).to.equal('070159bf-bd33-4998-b874-6b9c2bafe7fb');
+    expect(notes[0].props.key).to.equal('9233c2ae-7bad-41f5-9295-e73f0437295b');
+    expect(notes[1].props.key).to.equal('070159bf-bd33-4998-b874-6b9c2bafe7fb');
 
   });
 
