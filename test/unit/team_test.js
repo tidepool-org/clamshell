063abb834ee77552d7ae95afad959f26402c6788
Jamie <jamie@tidepool.org>
2014-02-17 15:00:55 +1300

    move core team functions so they are tested

---
+++ test/unit/team_test.js
@@ -0,0 +1,34 @@
+/*
+== BSD2 LICENSE ==
+Copyright (c) 2014, Tidepool Project
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the associated License, which is identical to the BSD 2-Clause
+License as published by the Open Source Initiative at opensource.org.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the License for more details.
+
+You should have received a copy of the License along with this program; if
+not, you can obtain one from Tidepool Project at tidepool.org.
+== BSD2 LICENSE ==
+*/
+
+var chai = require('chai');
+var expect = chai.expect;
+
+var team = require('../../src/core/team');
+var teamData = require('../../demo/data').team;
+
+describe('team', function() {
+
+    it('getThread returns notes in a thread', function() {
+
+        var thread = team.getThread(teamData,'9233c2ae-7bad-41f5-9295-e73f0437295b');
+        expect(thread).to.exist;
+        expect(thread).to.be.a('array');
+        expect(thread.length).to.equal(4);
+    });
+
+});
