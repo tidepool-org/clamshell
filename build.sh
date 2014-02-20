ad949080acff9145745e81465067c5727d2af6d8
Jamie <jamie@tidepool.org>
2014-02-20 22:44:52 +1300

    delete build dir

--- build.sh
+++ build.sh
@@ -1,15 +1,9 @@
 #! /bin/bash -eu
 
-BUILD_DIR=build
-
-function cleanup {
-  echo "Deleting the build directory: ${BUILD_DIR}"
-  rm -r ${BUILD_DIR};
-}
-
 rm -rf node_modules
+rm -rf build
 npm install .
-npm install -g react-tools
+sudo npm install -g react-tools
 
 # run tests
 ./node_modules/.bin/grunt test
