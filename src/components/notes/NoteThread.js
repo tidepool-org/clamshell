70ef127e4ba85f75383161a4c184a6a68584bbf7
Jamie <jamie@tidepool.org>
2014-03-12 20:21:48 +1300

    basic list styling

--- src/components/notes/NoteThread.js
+++ src/components/notes/NoteThread.js
@@ -47,8 +47,8 @@ var NoteThread = React.createClass({
       /* jshint ignore:start */
       <Note
       ref='commentNote'
-      imgColumns='col-xs-3 col-xs-offset-1 col-sm-1 col-sm-offset-1'
-      detailColumns='col-xs-8 col-sm-10'
+      imgColumns='col-xs-3 col-sm-1'
+      detailColumns='col-xs-9 col-sm-11'
       image='note-image'
       key={message.id}
       author={message.username}
