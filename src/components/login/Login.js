7f43f84a63152bfbf252f0207b8c6b21cbac41f9
Jamie <jamie@tidepool.org>
2014-04-23 21:53:28 +1200

    show logging in state

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -51,6 +51,8 @@ var Login = React.createClass({
     );
   },
   renderSignInForm: function(){
+    var submitButtonText = this.state.loggingIn ? 'Logging in...' : 'Log in';
+
     return (
       /* jshint ignore:start */
       <form className='login-form form-horizontal' role='form'>
@@ -66,7 +68,7 @@ var Login = React.createClass({
         </div>
         <div className='form-group'>
           <div className='col-xs-offset-2 col-xs-8 col-sm-offset-4 col-sm-4'>
-            <a type='submit' className='btn btn-default pull-right' ref='loginBtn' onClick={this.handleLogin}>Sign in</a>
+            <a type='submit' className='btn btn-default pull-right' ref='loginBtn' onClick={this.handleLogin}>{submitButtonText}</a>
           </div>
         </div>
       </form>
@@ -76,7 +78,6 @@ var Login = React.createClass({
   renderMessage: function() {
     var message = this.state.message;
     if (message) {
-
       return (
          /* jshint ignore:start */
           <div className='col-xs-offset-2 col-xs-8 col-sm-offset-4 col-sm-4 login-message js-login-message'>
